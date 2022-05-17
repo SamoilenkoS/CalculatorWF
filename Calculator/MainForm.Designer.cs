bcf62483354c6c6fafc1e6f01b8521ce140d32f2
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-05-17 18:55:04 +0300

    Add project files.

---
+++ Calculator/MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace Calculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.comboBoxOperator = new System.Windows.Forms.ComboBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(35, 36);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 22);
+            this.textBoxA.TabIndex = 0;
+            this.textBoxA.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxB_KeyDown);
+            // 
+            // comboBoxOperator
+            // 
+            this.comboBoxOperator.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOperator.FormattingEnabled = true;
+            this.comboBoxOperator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/",
+            "%"});
+            this.comboBoxOperator.Location = new System.Drawing.Point(160, 36);
+            this.comboBoxOperator.Name = "comboBoxOperator";
+            this.comboBoxOperator.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxOperator.TabIndex = 1;
+            this.comboBoxOperator.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxB_KeyDown);
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(297, 36);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 22);
+            this.textBoxB.TabIndex = 2;
+            this.textBoxB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxB_KeyDown);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(32, 88);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(52, 17);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.Text = "Result:";
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(160, 77);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(237, 28);
+            this.buttonCalculate.TabIndex = 4;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 119);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.comboBoxOperator);
+            this.Controls.Add(this.textBoxA);
+            this.Name = "MainForm";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.ComboBox comboBoxOperator;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonCalculate;
+    }
+}
+
---
+++ Calculator/MainForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            comboBoxOperator.SelectedIndex = 0;
+        }
+
+        private int CalculateResult(int a, int b, char operation)
+        {
+            int result = 0;
+            checked
+            {
+                try
+                {
+                    switch (operation)
+                    {
+                        case '+':
+                            result = a + b;
+                            break;
+                        case '-':
+                            result = a - b;
+                            break;
+                        case '*':
+                            result = a * b;
+                            break;
+                        case '/':
+                            result = a / b;
+                            break;
+                        case '%':
+                            result = a % b;
+                            break;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    throw;
+                }
+
+                return result;
+            }
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            if(!int.TryParse(textBoxA.Text, out var a)
+                || !int.TryParse(textBoxB.Text, out var b))
+            {
+                MessageBox.Show("Incorrect");
+                return;
+            }
+
+            var operation = comboBoxOperator.SelectedItem.ToString()[0];
+            try
+            {
+                int result = CalculateResult(a, b, operation);
+                labelResult.Text = result.ToString();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show(
+                      "Values too high",
+                      "Warning",
+                      MessageBoxButtons.OK,
+                      MessageBoxIcon.Warning);
+            }
+
+            //var result = MessageBox.Show("Hello world!", "Caption", MessageBoxButtons.YesNoCancel);
+            //if(result == DialogResult.Yes)
+            //{
+            //    MessageBox.Show("Yes");
+            //}
+        }
+
+        private void textBoxB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonCalculate_Click(sender, e);
+            }
+        }
+    }
+}
